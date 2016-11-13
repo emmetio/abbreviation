c68a8bbfe96cac60241e7123fa2c91624b84a20a
Sergey Chikuyonok <serge.che@gmail.com>
2016-11-14 00:33:45 +0300

    Expand abbreviation

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
-export { default as parse } from './lib/parser/index';
+import parse from './lib/parser/index';
+
+/**
+ * Parses given abbreviation and un-rolls it into a full tree: recursively
+ * replaces repeated elements with actual nodes
+ * @param  {String} abbr
+ * @return {Node}
+ */
+export default function(abbr) {
+	const tree = parse(abbr);
+	tree.walk(unroll);
+	return tree;
+}
+
+function unroll(node) {
+	if (!node.repeat || !node.repeat.count) {
+		return;
+	}
+
+	for (let i = 1; i < node.repeat.count; i++) {
+		const clone = node.clone(true);
+		clone.repeat.value = i;
+		clone.walk(unroll);
+		node.parent.insertBefore(clone, node);
+	}
+
+	node.repeat.value = node.repeat.count;
+}
--- test/parser.js
+++ test/parser.js
@@ -2,28 +2,37 @@
 
 const assert = require('assert');
 require('babel-register');
-const parse = require('../lib/parser').default;
+const parser = require('../lib/parser').default;
+const expander = require('../index').default;
 const stringify = require('./assets/stringify').default;
 
 describe('Parser', () => {
-	function abbr(str) {
-		return stringify(parse(str));
-	}
+	const parse = str => stringify(parser(str));
+	const expand = str => stringify(expander(str));
 
-	it('basic abbreviations', () => {
-		assert.equal(abbr('a>b'), '<a><b></b></a>');
-		assert.equal(abbr('a+b'), '<a></a><b></b>');
-		assert.equal(abbr('a+b>c+d'), '<a></a><b><c></c><d></d></b>');
-		assert.equal(abbr('a>b>c+e'), '<a><b><c></c><e></e></b></a>');
-		assert.equal(abbr('a>b>c^d'), '<a><b><c></c></b><d></d></a>');
-		assert.equal(abbr('a>b>c^^^^d'), '<a><b><c></c></b></a><d></d>');
+	describe('Parse', () => {
+		it('basic abbreviations', () => {
+			assert.equal(parse('a>b'), '<a><b></b></a>');
+			assert.equal(parse('a+b'), '<a></a><b></b>');
+			assert.equal(parse('a+b>c+d'), '<a></a><b><c></c><d></d></b>');
+			assert.equal(parse('a>b>c+e'), '<a><b><c></c><e></e></b></a>');
+			assert.equal(parse('a>b>c^d'), '<a><b><c></c></b><d></d></a>');
+			assert.equal(parse('a>b>c^^^^d'), '<a><b><c></c></b></a><d></d>');
+		});
+
+		it('groups', () => {
+			assert.equal(parse('a>(b>c)+d'), '<a><b><c></c></b><d></d></a>');
+			assert.equal(parse('(a>b)+(c>d)'), '<a><b></b></a><c><d></d></c>');
+			assert.equal(parse('a>((b>c)(d>e))f'), '<a><b><c></c></b><d><e></e></d><f></f></a>');
+			assert.equal(parse('a>((((b>c))))+d'), '<a><b><c></c></b><d></d></a>');
+			assert.equal(parse('a>(((b>c))*4)+d'), '<a>(<b><c></c></b>)*4<d></d></a>');
+		});
 	});
 
-	it('groups', () => {
-		assert.equal(abbr('a>(b>c)+d'), '<a><b><c></c></b><d></d></a>');
-		assert.equal(abbr('(a>b)+(c>d)'), '<a><b></b></a><c><d></d></c>');
-		assert.equal(abbr('a>((b>c)(d>e))f'), '<a><b><c></c></b><d><e></e></d><f></f></a>');
-		assert.equal(abbr('a>((((b>c))))+d'), '<a><b><c></c></b><d></d></a>');
-		assert.equal(abbr('a>(((b>c))*4)+d'), '<a>(<b><c></c></b>)*4<d></d></a>');
+	describe('Expand', () => {
+		it('unroll repeated elements', () => {
+			assert.equal(expand('a*2>b*3'), '<a*2@1><b*3@1></b><b*3@2></b><b*3@3></b></a><a*2@2><b*3@1></b><b*3@2></b><b*3@3></b></a>');
+			assert.equal(expand('a>(b+c)*2'), '<a>(<b></b><c></c>)*2@1(<b></b><c></c>)*2@2</a>');
+		});
 	});
 });
